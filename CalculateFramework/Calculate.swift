ef8c1b3e3e018ef1bb0a9b3a6ec84c7ae24d0d68
Madhavi Yamani <>
2020-03-25 07:04:30 -0700

    firstcommit

---
+++ CalculateFramework/Calculate.swift
@@ -0,0 +1,15 @@
+//
+//  Calculate.swift
+//  CalculateFramework
+//
+//  Created by Madhavi Yamani on 3/25/20.
+//  Copyright © 2020 info. All rights reserved.
+//
+
+import Foundation
+
+public class Calculate {
+    public func add (a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
---
+++ CalculateFramework/CalculateFramework.h
@@ -0,0 +1,19 @@
+//
+//  CalculateFramework.h
+//  CalculateFramework
+//
+//  Created by Madhavi Yamani on 3/25/20.
+//  Copyright © 2020 info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalculateFramework.
+FOUNDATION_EXPORT double CalculateFrameworkVersionNumber;
+
+//! Project version string for CalculateFramework.
+FOUNDATION_EXPORT const unsigned char CalculateFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculateFramework/PublicHeader.h>
+
+
